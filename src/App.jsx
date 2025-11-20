635ad6fc387bcfa10ce13eaf7b1fdd9604dc8b46
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-11-20 23:13:11 +0530

    iit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,99 @@
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem/chains'
-import './App.css'
-import { useState } from 'react'
+import "./App.css";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+import { createPublicClient } from "viem";
+import { base, mainnet } from "viem/chains";
+import { createConfig , http, injected, useAccount, useBalance, useConnect, WagmiProvider } from 'wagmi'
+import { metaMask, safe } from "wagmi/connectors";
 
-const client = createPublicClient({
-  chain: mainnet ,
-  transport: http() ,
+export const config = createConfig({
+  chains: [mainnet , base] ,
+  connectors: [
+    injected() ,
+    metaMask() ,
+    safe() ,
+  ] ,
+  transports: [{
+    [mainnet.id] : http() ,
+    [base.id] : http() ,
+}] ,
 })
 
+const client = createPublicClient({
+  chain: mainnet,
+  transport: http(),
+});
+
+const queryClient = new QueryClient();
+
 function App() {
-  const [balance  , setBalance ] = useState("") ;
-  async function getBalance() {
-    
-    const res = await client.getBalance({address :"0x27ddbd060fc0eca935b560f21e18c95fb01262cb"})
-    console.log(res)
-    setBalance(res.toString())
-  }
   return (
-    <>
-     {balance ? <p>the balance is {balance}</p> : null}
-      <button onClick={getBalance}>Get balance</button>     
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Balance />
+        <WalletAdapter />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+function WalletConnector() {
+  const { connectors, connect } = useConnect();
+  const { address, isConnected } = useAccount();
+
+  return (
+    <div>
+      {isConnected ? (
+        <p>Connected to {address}</p>
+      ) : (
+        connectors.map((connector) => (
+          <button
+            key={connector.uid}
+            onClick={() => {
+              connect({ connector });
+            }}
+          >
+            {connector.name}
+          </button>
+        ))
+      )}
+      <div>
+        {isConnected ? <p>you are connected</p> : <p>you are not connected</p>}
+      </div>
+    </div>
+  );
+}
+
+
+function WalletAdapter() {
+  const balance = useBalance()
+  return(<div>
+    <WalletConnector />
+    <p>Your balance: {balance.data?.formatted}</p>
+    <input type="text" placeholder="Address" />
+    <button>Send Eth </button>
+  </div>
   )
 }
 
-export default App
+function Balance() {
+  const {address} = useAccount();
+  async function getBalance() {
+    return client.getBalance({
+      address: address,
+    });
+  }
+
+  const { isPending, data: balance } = useQuery({
+    queryKey: ["balance"],
+    queryFn: getBalance,
+  });
+
+  return (
+    <div>
+      {!isPending && balance && <p>Balance: {balance.toString()}</p>}
+      <button onClick={getBalance}>Get balance</button>
+    </div>
+  );
+}
+
+export default App;
